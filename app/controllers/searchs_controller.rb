5cafb4e796033f979caa277e86fff0c39b7f4cd6
Your Name <iampatel.sanjana@gmail.com>
2023-10-03 15:46:07 +0530

    add some changes on search functionality

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -2,7 +2,10 @@ class SearchsController < ApplicationController
   def search
     @query=params[:query]
     @products=Product.where("products.product_name LIKE ?",["%#{@query}%"])
+    if @products.present?
     render json: @products, status: :ok
+    else  
+      render json: "Sorry, this is not available ............... "
+    end
   end
-
 end
